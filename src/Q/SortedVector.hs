311536ee1d45aafdcdf62e81a16c2b0db99aa6ec
Ghais <ghais.issa@gmail.com>
2021-07-09 12:16:03 -0400

    Update version to 0.0.0.5

--- src/Q/SortedVector.hs
+++ src/Q/SortedVector.hs
@@ -9,18 +9,26 @@ module Q.SortedVector
   , maxElement
   ) where
 
-import qualified Data.Vector.Algorithms.Merge  as V (sort)
+import qualified Data.Vector.Algorithms.Merge  as Merge (sort)
 
-import           Data.Vector.Storable  (Storable)
-import qualified Data.Vector.Storable as V (Vector (..), fromList, length, head, last, modify)
-import           Q.Types
+import qualified Data.Vector.Storable as V (Storable, Vector, fromList, head, last, modify)
 
+
+-- | A sorted vector.
 newtype SortedVector a = SortedVector (V.Vector a)
 
-fromList as = SortedVector (V.modify V.sort $ V.fromList as)
-fromVector v = SortedVector (V.modify V.sort v)
+-- | construct a 'SortedVector' from a list
+fromList :: (V.Storable a, Ord a) => [a] -> SortedVector a
+fromList as = SortedVector (V.modify Merge.sort $ V.fromList as)
+
+fromVector :: (V.Storable a, Ord a) => V.Vector a -> SortedVector a
+fromVector v = SortedVector (V.modify Merge.sort v)
+
+fromSortedList :: V.Storable a => [a] -> SortedVector a
 fromSortedList xs = SortedVector $ V.fromList xs
 
 
+minElement :: V.Storable a => SortedVector a -> a
 minElement (SortedVector v) = V.head v
+maxElement :: V.Storable a => SortedVector a -> a
 maxElement (SortedVector v) = V.last v
