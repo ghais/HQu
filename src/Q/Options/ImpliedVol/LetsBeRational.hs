59fe43436cb2c6e5a0d42f52b30b2394bea6cd50
Ghais <ghais.issa@gmail.com>
2021-08-23 20:56:35 -0400

    Seperate LBR to its own library. This makes it easier to work with GHCi

--- src/Q/Options/ImpliedVol/LetsBeRational.hs
+++ src/Q/Options/ImpliedVol/LetsBeRational.hs
@@ -4,20 +4,12 @@ module Q.Options.ImpliedVol.LetsBeRational (
 ) where
 
 import           Data.Coerce (coerce)
-import           Foreign.C.Types
-import           Q.Types
 import Q.Options.Black76 (B76Monad)
-
-
-
-
-
-foreign import ccall
-   "lets_be_rational.h implied_volatility_from_a_transformed_rational_guess" c_lbr ::
-     CDouble -> CDouble  -> CDouble -> CDouble  -> CDouble  -> CDouble
+import Q.Types
+import LetsBeRational
 
 -- | Calculate implied volatility for a European option using Let's Be Rational.
 euImpliedVol :: OptionType -> Forward -> Strike -> YearFrac -> Rate -> Premium -> B76Monad Vol
 euImpliedVol cp (Forward f) (Strike k) (YearFrac t) (Rate r) (Premium p) =
-  return $ coerce $ c_lbr (CDouble undiscountP) (CDouble f) (CDouble k) (CDouble t) (CDouble (cpi cp))
-  where undiscountP = undiscount  (discountFactor (coerce t) (coerce r)) p
+  return $ coerce $ lbr (cpi cp) f k t undiscountedP
+  where undiscountedP = undiscount  (discountFactor (coerce t) (coerce r)) p
